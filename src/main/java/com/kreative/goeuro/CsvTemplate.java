21f4ae58f86b917ba3da4f3c6ac49cac6bd210fb
classy-madafaqa <ahmedd.abd.allah@gmail.com>
2016-07-22 18:53:23 +0200

    starting up a new project

---
+++ src/main/java/com/kreative/goeuro/CsvTemplate.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kreative.goeuro;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class CsvTemplate {
+
+    APICaller api = new APICaller();
+
+    void generate(String aCity) {
+    }
+}
---
+++ src/main/java/com/kreative/goeuro/GoEuro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kreative.goeuro;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class GoEuro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HashSet<String> cities = new HashSet<>();
+        CsvTemplate csv = new CsvTemplate();
+
+        for (String aCity : args) {
+            if (!aCity.isEmpty() && !cities.contains(aCity)) {
+                cities.add(aCity);
+                csv.generate(aCity);
+            }
+        }
+    }
+
+}
